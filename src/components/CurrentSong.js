1a8b0dbfc32451d7ef1eef3df2b10a9ce5733932
Natalia- W158 <you@example.com>
2020-07-03 18:43:09 +0200

    iconsadded

---
+++ src/components/CurrentSong.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const CurrentSong = () => {
+    return ( 
+        <div>
+            <video src="https://www.youtube.com/watch?v=6M6samPEMpM"></video>
+        </div>
+        <div class="controls">
+            <img src="public/icons8-fast-backward-50.png"></img>
+            <img src="public/icons8-play-button-circled-50.png"></img>
+            <img src="public/icons8-fast-forward-50.png"></img>
+        </div>
+
+
+     );
+}
+ 
+export default CurrentSong;
