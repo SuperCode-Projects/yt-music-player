fe069c4687c7d4c0a52e1092f161bb92f2e3dc2b
Oliver-sehralt <schlueter1895@gmail.com>
2020-07-03 11:56:08 +0200

    added browserrouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
+import Search from "./components/Search";
+import Lyrics from "./components/Lyrics";
+import Events from "./components/Events";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>YT Music Player</h1>
+      		<BrowserRouter>
+			<div>
+				<nav>
+					<h1>YT Music Player</h1>
+				</nav>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/search">
+						<Search />
+					</Route>
+					<Route path="/lyrics" exact>
+						<Lyrics />
+					</Route>
+          <Route path="/events" exact>
+						<Events />
+					</Route>
+      </Switch>
+			</div>
+		</BrowserRouter>
     </div>
   );
 }
