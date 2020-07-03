0169ba5b36da1654073e0788fe44e54eb1e69ccb
Natalia- W158 <you@example.com>
2020-07-03 19:32:08 +0200

    finetuning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './App.css';
 import Search from "./components/Search";
 import Lyrics from "./components/Lyrics";
@@ -12,11 +13,26 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      		<BrowserRouter>
+			<div>
+        <Navbar />
+				<nav>
 					<h1>YT Music Player</h1>
-	         <Navbar />
+				</nav>
+				<Switch>
+       { /*
+					<Route exact path="/search">
 						<Search />
+       </Route> */}
+					<Route path="/lyrics" exact>
 						<Lyrics />
+					</Route>
+          <Route path="/events" exact>
 						<Events />
+					</Route>
+      </Switch>
+			</div>
+		</BrowserRouter>
     </div>
   );
 }
---
+++ src/components/NextUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const NextUp = () => {
+    return ( 
+        <div className="NextUp">
+            <h5>Next Up</h5>
+            <img className="AlbumCover" src="public/logo192.png"></img>
+            <p>Spice Girls - Wannabe</p>
+        </div>
+     );
+}
+ 
+export default NextUp;
