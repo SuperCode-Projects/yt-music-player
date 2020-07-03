0e11bc6354a8153b061f45866409a71f0ab7f0c2
Natalia- W158 <you@example.com>
2020-07-03 20:46:05 +0200

    added easy Css

--- src/components/CurrentSong.js
+++ src/components/CurrentSong.js
@@ -1,21 +1,17 @@
 import React from "react";
 
+import './CurrentSong.css';
+
 const CurrentSong = () => {
     return ( 
      <div className="CurrentSong">
         <div>
-            <video src="https://www.youtube.com/watch?v=6M6samPEMpM" alt="video"></video>
+            <video className="video" src="https://www.youtube.com/watch?v=6M6samPEMpM" alt="video">Video</video>
         </div>
         <div className="controls">
-<<<<<<< HEAD
             <img src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
             <img src="icons8-play-button-circled-50.png" alt="playButton"></img>
             <img src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
-=======
-            <img src="/icons8-fast-forward-50.png" alt="backwardsIcon"></img>
-            <img src="/icons8-play-button-circled-50.png" alt="playButton"></img>
-            <img src="/icons8-fast-forward-50.png" alt="backwardsIcon"></img>
->>>>>>> cb86ae9b93d9016d70f395bd1f8aee1e71a3fa64
         </div>
         <div className="NowPlaying">
             <p>The Backstreet Boys - Everybody</p>
--- src/components/NextUp.js
+++ src/components/NextUp.js
@@ -1,14 +1,11 @@
 import React from "react";
+import './NextUp.css';
 
 const NextUp = () => {
     return ( 
         <div className="NextUp">
             <h5>Next Up</h5>
-<<<<<<< HEAD
-            <img className="AlbumCover" src="logo192.png" alt="albumcoverimage"></img>
-=======
-            <img className="AlbumCover" src="/logo192.png" alt="albumcoverimage"></img>
->>>>>>> cb86ae9b93d9016d70f395bd1f8aee1e71a3fa64
+            <img className="AlbumCoverNextUp" src="logo192.png" alt="albumcoverimage"></img>
             <p>Spice Girls - Wannabe</p>
         </div>
      );
