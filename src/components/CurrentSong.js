ac84b57f3492936960fd8ea3681dd80cc8de698d
Oliver-sehralt <schlueter1895@gmail.com>
2020-07-03 20:51:45 +0200

    changed search.css

--- src/components/CurrentSong.js
+++ src/components/CurrentSong.js
@@ -7,15 +7,9 @@ const CurrentSong = () => {
             <video src="https://www.youtube.com/watch?v=6M6samPEMpM" alt="video"></video>
         </div>
         <div className="controls">
-<<<<<<< HEAD
             <img src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
             <img src="icons8-play-button-circled-50.png" alt="playButton"></img>
             <img src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
-=======
-            <img src="/icons8-fast-forward-50.png" alt="backwardsIcon"></img>
-            <img src="/icons8-play-button-circled-50.png" alt="playButton"></img>
-            <img src="/icons8-fast-forward-50.png" alt="backwardsIcon"></img>
->>>>>>> cb86ae9b93d9016d70f395bd1f8aee1e71a3fa64
         </div>
         <div className="NowPlaying">
             <p>The Backstreet Boys - Everybody</p>
