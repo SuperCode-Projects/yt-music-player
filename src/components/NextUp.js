dba7b2f66911fd40320021851a8d164b3125310c
Oliver-sehralt <schlueter1895@gmail.com>
2020-07-03 21:07:59 +0200

    chamged Lyrics.js

--- src/components/NextUp.js
+++ src/components/NextUp.js
@@ -5,11 +5,7 @@ const NextUp = () => {
     return ( 
         <div className="NextUp">
             <h5>Next Up</h5>
-<<<<<<< HEAD
             <img className="AlbumCover" src="logo192.png" alt="albumcoverimage"></img>
-=======
-            <img className="AlbumCoverNextUp" src="logo192.png" alt="albumcoverimage"></img>
->>>>>>> 0e11bc6354a8153b061f45866409a71f0ab7f0c2
             <p>Spice Girls - Wannabe</p>
         </div>
      );
