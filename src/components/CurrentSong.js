97e0416e10781e9ebaafa70f572630057397bb9a
Natalia- W158 <you@example.com>
2020-07-03 18:49:20 +0200

    added NowPlaying

--- src/components/CurrentSong.js
+++ src/components/CurrentSong.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 
 const CurrentSong = () => {
     return ( 
+     <div className="CurrentSong">
         <div>
             <video src="https://www.youtube.com/watch?v=6M6samPEMpM"></video>
         </div>
-        <div class="controls">
+        <div className="controls">
             <img src="public/icons8-fast-backward-50.png"></img>
             <img src="public/icons8-play-button-circled-50.png"></img>
             <img src="public/icons8-fast-forward-50.png"></img>
         </div>
+        <div className="NowPlaying">
+            <p>The Backstreet Boys - Everyboday</p>
+        </div>
+
+     </div>
 
 
      );
