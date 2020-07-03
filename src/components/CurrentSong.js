7a435fbbf777288618e7c6d192013ec677743215
Natalia- W158 <you@example.com>
2020-07-03 22:02:10 +0200

    added css to buttons

--- src/components/CurrentSong.js
+++ src/components/CurrentSong.js
@@ -9,9 +9,9 @@ const CurrentSong = () => {
         <iframe width="560" height="315" src="https://www.youtube.com/embed/6M6samPEMpM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <div className="Controls">
-            <img className="Backwards" src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
-            <img className="Play" src="icons8-play-button-circled-50.png" alt="playButton"></img>
-            <img className="Forewards" src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
+            <img className="Button" id="Backwards" src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
+            <img className="Button" id="Play" src="icons8-play-button-circled-50.png" alt="playButton"></img>
+            <img className="Button" id="Forewards" src="icons8-fast-forward-50.png" alt="backwardsIcon"></img>
         </div>
         <div className="NowPlaying">
             <p>The Backstreet Boys - Everybody</p>
