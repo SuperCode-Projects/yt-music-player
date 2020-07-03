12c0acb5575bab4b66dc0504aa45631b732c57c0
Oliver-sehralt <schlueter1895@gmail.com>
2020-07-03 13:24:55 +0200

    added Search Lyrics Events js

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,7 +3,15 @@ import React from "react";
 import '/Events.css';
 
 
-
+const Events = () => (
+    <div className="events">
+        <img src = "images/band1.jpg"></img>
+        <h2>The Weeknd</h2>
+        <h3>Lanxess Arena Köln</h3>
+        <p>05.11.2021</p>
+        <img src = "images/tickets.png"></img>
+    </div>
+);
 
 
 export default Events;
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,5 +2,31 @@ import React from "react";
 
 import '/Search.css';
 
+const Search = () => (
+    <section>
+        <form>
+  <label for="name"></label>
+  <input type="text" id="name" name="name" placeholder="Type in the artists name"></input>
+      </form>
+      <div className="songs">
+          <article>
+              <img src = "images/band1.jpg"></img>
+              <h2>The Weeknd - Blinding Lights</h2>
+              <p>The Weeknd</p>
+             </article>
+             <article>
+              <img src = "images/band2.jpg"></img>
+              <h2>The Weeknd - If your eyes</h2>
+              <p>The Weeknd</p>
+             </article>
+             <article>
+              <img src = "images/band3.jpg"></img>
+              <h2>The Weeknd - I feel it comming</h2>
+              <p>The Weeknd</p>
+             </article>
+         </div>
+    </section>
+)
+
 
 export default Search;
