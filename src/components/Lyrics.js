78f6d899cc923297f89847d6dd02bb6744afa52f
Oliver-sehralt <schlueter1895@gmail.com>
2020-07-11 11:24:17 +0200

    changed Lyrics.js

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -9,28 +9,29 @@ class Lyrics extends Component {
     super(props);
     this.state = {
       q: "",
-      lyrics: [],
+      lyrics: "",
     };
+    this.getLyrics = this.getLyrics.bind(this);
   }
 
-  getLyrics(){
+  getLyrics() {
     fetch(
-        `https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.result.id}&apikey=${musixmatchkey}`
-      )
-        .then((response) => response.json())
-        .then((result) => console.log(result));
-    }
-
-
+      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_artist=Michael%20Jackson&q_track=Thriller&apikey=${musixmatchkey}`
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        this.setState({ lyrics: result.message.body.lyrics.lyrics_body });
+      });
+  }
 
   render() {
     return (
       <>
-      <button onClick={this.getLyrics}>get Lyrics</button>
+        <button onClick={this.getLyrics}>get Lyrics</button>
         <Link to="/">Go Back</Link>
         <div className="lyricsBody">
-          <p>{this.state.lyrics.lyrics_body}</p>
-    <p>{JSON.stringify(this.props.video)}</p>
+          <p>{this.state.lyrics}</p>
         </div>
       </>
     );
