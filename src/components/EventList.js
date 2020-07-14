0892af05c08710332d576066766216d2d4cf3edc
Oliver-sehralt <schlueter1895@gmail.com>
2020-07-14 17:30:13 +0200

    added date and time at EventList.js

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -18,9 +18,13 @@ class EventList extends Component {
     return (
       <div className="SearchlistEvents">
         <a href={this.props.event.url} target="_blank">
-        <><img id="concert" src="concert.jpg"/></>
+          <>
+            <img id="concert" src="concert.jpg" />
+          </>
           {this.props.event.name}
         </a>
+        Date: {this.props.event.dates.start.localDate}
+        City: {this.props.event.dates.timezone}
         {/* <a href="#" onClick={this.handleEventClicked}>
                 <img src={this.props.item.events.images[0]} alt="image"/> 
                 <>{this.props.item.discovery.v2.events.name}
